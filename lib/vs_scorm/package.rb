b14f451ec47b03db9a3ab44d5743cf8ab4b4b199
Lode Cools <lode.cools@venturespirit.com>
2018-06-07 12:25:58 +0200

    Cleanup the dry-run and cleanup options

--- lib/vs_scorm/package.rb
+++ lib/vs_scorm/package.rb
@@ -1,7 +1,6 @@
 require 'zip'
 require 'fileutils'
 require 'open-uri'
-require 'vs_scorm/datatypes'
 require 'vs_scorm/manifest'
 
 module VsScorm
@@ -17,10 +16,8 @@ module VsScorm
     attr_accessor :package    # The file name of the package file.
     
     DEFAULT_LOAD_OPTIONS = { 
-      :strict => false,
-      :dry_run => false, 
+      :strict => true,
       :cleanup => true,
-      :force_cleanup => false,
       :name => nil,
       :repository => nil
     }
@@ -42,7 +39,6 @@ module VsScorm
     # block. The available options are:
     #
     #   :+strict+:     If +false+ the manifest will be parsed in a nicer way. Default: +true+.
-    #   :+dry_run+:    If +true+ nothing will be written to the file system. Default: +false+.
     #   :+cleanup+:    If +false+ no cleanup will take place if an error occur. Default: +true+.
     #   :+name+:       The name to use when extracting the package to the 
     #                  repository. Default: will use the filename of the package 
@@ -51,14 +47,10 @@ module VsScorm
     #
     def initialize(filename, options = {}, &block)
       @options = DEFAULT_LOAD_OPTIONS.merge(options)
-      @package = filename.respond_to?(:path) ? filename.path : filename
+      @package = filename
       
-      # Check if package is a directory or a file.
-      if File.directory?(@package)
-        @name = File.basename(@package)
-        @repository = File.dirname(@package)
-        @path = File.expand_path(@package)
-      else
+      # Check if package is a file.
+      if File.file?(@package)
         i = nil
         begin
           # Decide on a name for the package.
@@ -74,14 +66,16 @@ module VsScorm
 
         # Make sure the generated path is unique.
         end while File.exists?(@path)
+      else
+        raise InvalidPackage, "The package must be a zip file!"
       end
       
       # Extract the package
       extract!
                                                         
       # Detect and read imsmanifest.xml
-      if exists?('imsmanifest.xml')
-        @manifest = Manifest.new(self, file('imsmanifest.xml'))
+      if File.exists?(File.join(@path, 'imsmanifest.xml'))
+        @manifest = Manifest.new(self, File.read(File.join(@path, 'imsmanifest.xml')))
       else
         raise InvalidPackage, "#{File.basename(@package)}: no imsmanifest.xml, maybe not SCORM compatible?"
       end
@@ -91,42 +85,28 @@ module VsScorm
       
       # Make sure the package is closed when the caller has finished reading it.
       self.close
+      self.cleanup if @options[:cleanup]
 
     # If an exception occur the package is auto-magically closed and any 
     # residual data deleted in a clean way.
     rescue Exception => e
       self.close
-      self.cleanup
+      self.cleanup if @options[:cleanup]
       raise e
     end
     
     # Closes the package.
     def close
       @zipfile.close if @zipfile
-      
-      # Make sure the extracted package is deleted if force_cleanup_on_close
-      # is enabled.
-      self.cleanup if @options[:force_cleanup_on_close]
     end
     
     # Cleans up by deleting all extracted files. Called when an error occurs.
     def cleanup
-      FileUtils.rmtree(@path) if @options[:cleanup] && !@options[:dry_run] && @path && File.exists?(@path) && package?
+      FileUtils.rmtree(@path) if @path && File.exists?(@path)
     end
     
-    # Extracts the content of the package to the course repository. This will be
-    # done automatically when opening a package so this method will rarely be
-    # used. If the +dry_run+ option was set to +true+ when the package was
-    # opened nothing will happen. This behavior can be overridden with the
-    # +force+ parameter. 
-    def extract!(force = false)
-      return if @options[:dry_run] && !force
-      
-      # If opening an already extracted package; do nothing.
-      if not package?
-        return
-      end
-      
+    # Extracts the content of the package to the course repository.
+    def extract!
       # Create the path to the course
       FileUtils.mkdir_p(@path)
       
@@ -135,58 +115,6 @@ module VsScorm
         entry_dir = File.dirname(entry_path)
         FileUtils.mkdir_p(entry_dir) unless File.exists?(entry_dir)
         entry.extract(entry_path)
-      end
-    end
-    
-    # This will only return +true+ if what was opened was an actual zip file.
-    # It returns +false+ if what was opened was a filesystem directory.
-    def package?
-      return false if File.directory?(@package)
-      return true
-    end
-    
-    # Reads a file from the package. If the file is not extracted yet (all files
-    # are extracted by default when opening the package) it will be extracted
-    # to the file system and its content returned. If the +dry_run+ option was
-    # set to +true+ when opening the package the file will <em>not</em> be
-    # extracted to the file system, but read directly into memory.
-    def file(filename)
-      if File.exists?(@path)
-        File.read(path_to(filename))
-      else
-        Zip::File.foreach(@package) do |entry|
-          return entry.get_input_stream {|io| io.read } if entry.name == filename
-        end
-      end
-    end
-    
-    # Returns +true+ if the specified file (or directory) exists in the package.
-    def exists?(filename)
-      if File.exists?(@path)
-        File.exists?(path_to(filename))
-      else
-        Zip::File::foreach(@package) do |entry|
-          return true if entry.name == filename
-        end
-        false
-      end
-    end
-    
-    # Computes the absolute path to a file in an extracted package given its
-    # relative path. The argument +relative+ can be used to get the path 
-    # relative to the course repository.
-    #
-    # Ex.
-    #    <tt>pkg.path => '/var/lms/courses/MyCourse/'</tt>
-    #    <tt>pkg.course_repository => '/var/lms/courses/'</tt>
-    #    <tt>path_to('images/myimg.jpg') => '/var/lms/courses/MyCourse/images/myimg.jpg'</tt>
-    #    <tt>path_to('images/myimg.jpg', true) => 'MyCourse/images/myimg.jpg'</tt>
-    #
-    def path_to(relative_filename, relative = false)
-      if relative
-        File.join(@name, relative_filename)
-      else
-        File.join(@path, relative_filename)
       end
     end
     
